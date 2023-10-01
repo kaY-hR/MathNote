9742f600a6ab0cf9a40c70d605befd1c871696a6
kaY-hR <harunorikawai@gmail.com>
2023-10-01 20:01:04 +0900

    copy用バッチファイルについて、実行開始時にdocsを削除するように修正

--- source/copy_htmls_to_docs.bat
+++ source/copy_htmls_to_docs.bat
@@ -1,23 +1,26 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM 移動先フォルダのパス
+set "destination=..\docs"
+
+REM 移動先フォルダ内のファイルを削除
+del /Q "!destination!\*.*"
+
 REM カレントディレクトリ内のHTMLファイルをループ処理
 for %%f in (*.html) do (
     REM コピー元のファイルパス
     set "source=%%~dpnxf"
     
-    REM コピー先のフォルダパス
-    set "destination=..\docs\%%~nxf"
-    
     REM ファイルをコピー
-    copy "!source!" "!destination!"
+    copy "!source!" "!destination!\%%~nxf"
 )
 
 REM コピー元のstyle.cssファイルパス
 set "css_source=style.css"
 
 REM コピー先のstyle.cssファイルパス
-set "css_destination=..\docs\style.css"
+set "css_destination=!destination!\style.css"
 
 REM style.cssファイルをコピー
 if exist "!css_source!" (
