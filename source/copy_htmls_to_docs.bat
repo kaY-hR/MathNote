c71edecff004b5f4b1b7d1d30b02839f6f838065
kaY-hR <harunorikawai@gmail.com>
2023-10-01 17:53:27 +0900

    sourceとdocに分離

---
+++ source/copy_htmls_to_docs.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM カレントディレクトリ内のHTMLファイルをループ処理
+for %%f in (*.html) do (
+    REM コピー元のファイルパス
+    set "source=%%~dpnxf"
+    
+    REM コピー先のフォルダパス
+    set "destination=..\docs\%%~nxf"
+    
+    REM ファイルをコピー
+    copy "!source!" "!destination!"
+    
+    echo コピー完了: "!source!" -> "!destination!"
+)
+
+endlocal
